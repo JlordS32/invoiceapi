a4d7b1b5d39053ba87b1078947e1fcdcc654c842
JlordS32 <jlordsty128@gmail.com>
2023-11-21 18:43:28 +1100

    added cors

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-// Import packages
 const express = require('express');
 const invoice = require('./routes/invoices');
+const cors = require('cors'); // Import the 'cors' package
 
 // Middlewares
 const app = express();
 app.use(express.json());
+app.use(cors()); // Enable CORS for all routes
 
 // Routes
 app.use('/invoices', invoice);
